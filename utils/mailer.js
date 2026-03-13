36d0ba3385b30686c0fe27049316bf41de49c97f
geccodingclub <geccodingclub@gmail.com>
2026-03-13 22:25:12 +0530

    fix email bug

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,6 +2,10 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  family: 4, // Force IPv4 to avoid Render ENETUNREACH errors
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
